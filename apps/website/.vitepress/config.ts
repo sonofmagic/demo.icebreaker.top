649cf27364d7d6bb8477e12ed739e349648fe696
ice breaker <1324318532@qq.com>
2024-11-06 14:07:41 +0800

    chore(deps): upgrade

--- apps/website/.vitepress/config.ts
+++ apps/website/.vitepress/config.ts
@@ -1,29 +1,74 @@
+import bodyParser from 'body-parser'
 import { defineConfig } from 'vitepress'
+
+function wait(timeout: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(undefined)
+    }, timeout)
+  })
+}
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   outDir: '../../dist',
-  title: 'icebreaker\'s monorepo',
-  description: 'icebreaker\'s monorepo. upgrade your monorepo',
+  title: 'icebreaker\'s demo website',
+  description: 'icebreaker\'s demo website',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '使用手册', link: '/' },
-      { text: '一些思考', link: '/thinking' },
     ],
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/sonofmagic/monorepo-template' },
+      { icon: 'github', link: 'https://github.com/sonofmagic/demo.icebreaker.top' },
     ],
     outline: {
       label: '目录',
       level: [2, 3],
     },
     logo: '/logo.jpg',
-    sidebar: {
-      '/': [
-        { text: '使用手册', link: '/' },
-        { text: '一些思考', link: 'thinking' },
-      ],
-    },
+    // sidebar: {
+    //   '/': [
+    //     { text: '使用手册', link: '/' },
+    //     { text: '一些思考', link: 'thinking' },
+    //   ],
+    // },
+  },
+  vite: {
+    plugins: [
+      {
+        name: 'icebreaker:api',
+        configureServer: {
+          handler(server) {
+            server.middlewares.use(bodyParser.json())
+            server.middlewares.use('/api/resolve', async (req, res) => {
+              if (typeof req.body.timeout === 'number') {
+                await wait(req.body.timeout)
+              }
+
+              res.setHeader('Content-Type', 'application/json')
+              res.end(JSON.stringify({ message: 'resolve', timeout: req.body.timeout, now: Date.now() }))
+            })
+
+            server.middlewares.use('/api/reject', async (req, res) => {
+              if (typeof req.body.timeout === 'number') {
+                await wait(req.body.timeout)
+              }
+              res.statusCode = 400
+              res.setHeader('Content-Type', 'application/json')
+              res.end(JSON.stringify({ message: 'reject', timeout: req.body.timeout, now: Date.now() }))
+            })
+          },
+        },
+      },
+    ],
   },
 })
+
+declare module 'http' {
+  interface IncomingMessage {
+    body: {
+      [key: string]: any
+    }
+  }
+}
---
+++ apps/website/promise-concurrency.md
@@ -0,0 +1,83 @@
+---
+layout: doc
+---
+
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+
+const allRes = ref([])
+const allSettledRes = ref([])
+
+function emitResolve (timeout:number){
+  return axios.post('/api/resolve', { timeout }).then(res=>res.data)
+}
+
+function emitReject (timeout:number){
+  return axios.post('/api/reject', { timeout }).then(res=>res.data)
+}
+
+async function all() {
+  try {
+    allRes.value = await Promise.all([
+      emitResolve(100),
+      emitResolve(200),
+      emitResolve(300)
+    ])
+  }
+  catch (err) {
+    allRes.value = [err]
+  }
+}
+
+async function allSettled(){
+  try {
+    allSettledRes.value = await Promise.allSettled([
+      emitResolve(100),
+      emitResolve(200),
+      emitResolve(300)
+    ])
+  }
+  catch (err) {
+    allSettledRes.value = [err]
+  }
+}
+</script>
+
+# promise 并发
+
+## [Promise.all()](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/all)
+
+`Promise.all()` 静态方法接受一个 Promise 可迭代对象作为输入，并返回一个 Promise。当所有输入的 Promise 都被兑现时，返回的 Promise 也将被兑现（即使传入的是一个空的可迭代对象），并返回一个包含所有兑现值的数组。如果输入的任何 Promise 被拒绝，则返回的 Promise 将被拒绝，并带有第一个被拒绝的原因。
+
+<button @click="all">emit</button>
+
+```jsonc-vue
+{{JSON.stringify(allRes,null,2)}}
+```
+
+<!-- ::: details 代码块 -->
+
+```ts-vue
+{{all.toString()}}
+```
+
+<!-- ::: -->
+
+## [Promise.allSettled()](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled)
+
+Promise.allSettled() 静态方法将一个 Promise 可迭代对象作为输入，并返回一个单独的 Promise。当所有输入的 Promise 都已敲定时（包括传入空的可迭代对象时），返回的 Promise 将被兑现，并带有描述每个 Promise 结果的对象数组。
+
+<button @click="allSettled">emit</button>
+
+```jsonc-vue
+{{JSON.stringify(allSettledRes,null,2)}}
+```
+
+<!-- ::: details 代码块 -->
+
+```ts-vue
+{{allSettled.toString()}}
+```
+
+<!-- ::: -->
